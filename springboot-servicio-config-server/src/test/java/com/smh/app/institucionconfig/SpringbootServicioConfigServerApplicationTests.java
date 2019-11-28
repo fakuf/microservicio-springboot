c924d558af757260d286e20caa05474fe8a35597
fakuf <faku_f@hotmail.com>
2019-11-28 11:08:29 -0300

    config server

---
+++ springboot-servicio-config-server/src/test/java/com/smh/app/institucionconfig/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smh.app.institucionconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
